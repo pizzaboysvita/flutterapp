a808e51ce23236883e6e600ec6021372c59f82cb
AbhiVK108 <endiabhiram@gmail.com>
2025-12-11 10:31:07 +0530

    IOS configure chnages are made to publish the app to appstore.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,15 +1,16 @@
 import UIKit
 import Flutter
-import GoogleMaps   // <- add this
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
 
-    GMSServices.provideAPIKey("AIzaSyAPlkDpeY1uNpGIu5YvAJ4oMcOM36qNCaA") // <- Add your key here
+    GMSServices.provideAPIKey("AIzaSyAPlkDpeY1uNpGIu5YvAJ4oMcOM36qNCaA")
 
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
